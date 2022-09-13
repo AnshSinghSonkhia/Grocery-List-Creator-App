11ded912a42697fb6d08f069ff176d2401f7fb2f
Ansh Singh Sonkhia <anshssonkhia@gmail.com>
2022-09-13 12:05:12 +0530

    First Commit

---
+++ app/src/main/java/com/sonkhiasinghansh/grocerylistmaking/GroceryDao.kt
@@ -0,0 +1,17 @@
+package com.sonkhiasinghansh.grocerylistmaking
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+
+interface GroceryDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(item: GroceryItems)
+    @Delete
+    suspend fun delete(item: GroceryItems)
+    @Query("SELECT * FROM Grocery_items")
+    fun getAllGroceryItems(): LiveData<List<GroceryItems>>
+
+
+}
