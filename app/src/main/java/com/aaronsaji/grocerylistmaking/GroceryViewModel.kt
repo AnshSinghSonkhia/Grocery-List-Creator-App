d49f9ffd517152527b41983ee21222a1307d873e
Ansh Singh Sonkhia <110414565+AnshSinghSonkhia@users.noreply.github.com>
2024-02-04 19:53:11 +0530

    Laptop to GitHub

---
+++ app/src/main/java/com/aaronsaji/grocerylistmaking/GroceryViewModel.kt
@@ -0,0 +1,15 @@
+package com.sonkhiasinghansh.grocerylistmaking
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class GroceryViewModel(private val repository: GroceryRepository):ViewModel() {
+    fun insert(items: GroceryItems) = GlobalScope.launch {
+        repository.insert(items)
+    }
+    fun delete(items: GroceryItems) = GlobalScope.launch {
+        repository.delete(items)
+    }
+    fun getAllGroceryItems() = repository.getAllItems()
+}
